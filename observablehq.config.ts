215a317062d9726d54edc49d3b22702e1d52754f
gvarnavi <gvarnavi@mit.edu>
2024-05-09 10:28:51 -0700

    adding footer

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -1,10 +1,18 @@
 // See https://observablehq.com/framework/config for documentation.
+
+const footer: string = `
+  <div>
+    One.MIT is a project of <a href="https://mitnano.mit.edu/" target="_blank">MIT.nano</a> <br>
+    Massachusetts Institute of Technology | 77 Massachusetts Avenue &ndash; Building 12 | Cambridge, MA 02139 | <a href="mailto:mitnano@mit.edu?subject=Inquiry about One.MIT">mitnano@mit.edu</a>
+  </div>
+`;
+
 export default {
   title: "One.MIT",
   theme: "light",
   root: "src",
   pager: false,
-  footer: "",
+  footer: footer,
   pages: [
 	  {
 		  name: "One.MIT 2018",
