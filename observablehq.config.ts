190b112372045daefebba8d60271cf72d0ecae7f
gvarnavi <gvarnavi@mit.edu>
2024-05-29 13:13:28 -0700

    add favicon

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -18,6 +18,13 @@ const header: string = `
 </div>
 `;
 
+const head: string = `
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+<link rel="manifest" href="/assets/site.webmanifest">
+`;
+
 export default {
   title: "One.MIT",
   theme: "light",
@@ -25,6 +32,7 @@ export default {
   pager: true,
   footer: footer,
   header: header,
+  head: head,
   typographer: true,
   pages: [
     {
